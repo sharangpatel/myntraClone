75511c534b3141959dfc8850d2aada0ad834ad19
sharangpatel <103412062+sharangpatel@users.noreply.github.com>
2024-04-26 02:09:26 +0530

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+let bagItems;
+onLoad()
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem('bagItems')
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage()
+  displayBagIcon()
+}
+
+function addToBag(itemId){
+  bagItems.push(itemId);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
+  displayBagIcon()
+}
+
+function displayBagIcon(){
+    let bagItemCount = document.querySelector('.bag-item-count');
+      if (bagItems.length > 0) {
+        bagItemCount.style.visibility='visible';
+        bagItemCount.innerHTML = bagItems.length
+      
+      } else{
+        bagItemCount.style.visibility='hidden';
+      }
+     
+}
+
+
+
+function displayItemsOnHomePage(){
+  let itemsContainerElement = document.querySelector( '.items-container' );
+  if(itemsContainerElement === null){
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src='${item.image}' alt="item-img">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}%)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
+
+
